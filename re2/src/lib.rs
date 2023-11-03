78902a74390eb56d794681b48d9e3b2cdc5eddb1
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2023-11-03 01:56:38 -0400

    remove sys subcrate

--- re2/src/lib.rs
+++ re2/src/lib.rs
@@ -3,9 +3,8 @@
 
 //! ???
 
-#![deny(unsafe_code)]
 // Turn all warnings into errors!
-// #![deny(warnings)]
+#![allow(warnings)]
 // Warn for missing docs in general, and hard require crate-level docs.
 // #![warn(missing_docs)]
 #![warn(rustdoc::missing_crate_level_docs)]
@@ -39,16 +38,69 @@
 /* Arc<Mutex> can be more clear than needing to grok Orderings: */
 #![allow(clippy::mutex_atomic)]
 
+mod bindings;
 
-pub fn add(left: usize, right: usize) -> usize { left + right }
+use bindings::root::re2;
 
-#[cfg(test)]
-mod tests {
-  use super::*;
+use std::{ffi::CStr, fmt, mem, ops, ptr, slice, str};
 
-  #[test]
-  fn it_works() {
-    let result = add(2, 2);
-    assert_eq!(result, 4);
+///```
+/// use re2::StringPiece;
+///
+/// let s = StringPiece::from_str("a");
+///
+/// assert_eq!(s.as_str(), "a");
+/// assert_eq!(&format!("{}", &s), "a");
+/// assert_eq!(&format!("{:?}", &s), "\"a\"");
+/// ```
+pub struct StringPiece(pub(crate) re2::StringPiece);
+
+impl StringPiece {
+  #[inline]
+  pub const fn from_str(s: &str) -> Self {
+    Self(re2::StringPiece {
+      data_: unsafe { mem::transmute(s.as_bytes().as_ptr()) },
+      size_: s.len(),
+    })
+  }
+
+  #[inline]
+  pub const fn as_str(&self) -> &str {
+    let Self(re2::StringPiece { data_, size_ }) = self;
+    let span: &[u8] = unsafe { slice::from_raw_parts(mem::transmute(*data_), *size_) };
+    unsafe { str::from_utf8_unchecked(span) }
+  }
+}
+
+impl fmt::Display for StringPiece {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { write!(f, "{}", self.as_str()) }
+}
+
+impl fmt::Debug for StringPiece {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { write!(f, "\"{}\"", self.as_str()) }
+}
+
+///```
+/// use re2::*;
+///
+/// let p = StringPiece::from_str("he");
+/// let r = RE2::new(&p);
+/// assert!(r.full_match(&StringPiece::from_str("he")));
+/// assert!(r.partial_match(&StringPiece::from_str("hello")));
+/// assert!(r.partial_match(&StringPiece::from_str("the")));
+///```
+pub struct RE2(pub(crate) re2::RE2);
+
+impl RE2 {
+  pub fn new(pattern: &StringPiece) -> Self { Self(unsafe { re2::RE2::new2(&pattern.0) }) }
+
+  #[inline]
+  pub fn full_match(&self, text: &StringPiece) -> bool {
+    unsafe { re2::RE2_FullMatchN(&text.0, &self.0, ptr::null(), 0) }
+  }
+
+  #[inline]
+  pub fn partial_match(&self, text: &StringPiece) -> bool {
+    unsafe { re2::RE2_PartialMatchN(&text.0, &self.0, ptr::null(), 0) }
   }
 }
