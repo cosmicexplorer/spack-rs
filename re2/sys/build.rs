73a743aff536790620e5bc46ddb752008eda321e
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2023-11-03 01:52:45 -0400

    make re2 crate

--- re2/sys/build.rs
+++ re2/sys/build.rs
@@ -40,5 +40,78 @@
 #![allow(clippy::mutex_atomic)]
 
 use bindgen;
+use spack::{
+  self,
+  utils::{self, prefix},
+  SpackInvocation,
+};
+use tokio::task;
 
-fn main() {}
+use std::{io, path::PathBuf};
+
+async fn link_libraries(prefix: prefix::Prefix) -> Result<(), prefix::PrefixTraversalError> {
+  let query = prefix::LibsQuery {
+    needed_libraries: vec![prefix::LibraryName("re2".to_string())],
+    kind: prefix::LibraryType::Static,
+  };
+  let libs = query.find_libs(&prefix).await?;
+
+  libs.link_libraries();
+
+  Ok(())
+}
+
+fn generate_bindings(
+  re2_prefix: PathBuf,
+  header_path: PathBuf,
+  output_path: PathBuf,
+) -> Result<(), io::Error> {
+  let re2_header_root = re2_prefix.join("include");
+
+  let bindings = bindgen::Builder::default()
+    /* FIXME: is there really not a more specific method than "clang_arg()" to add an include
+     * search dir??? */
+    .clang_arg(format!("-I{}", re2_header_root.display()))
+    .header(format!("{}", header_path.display()))
+    .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+    .clang_arg("-std=c++17")
+    .clang_args(&["-x", "c++"])
+    .enable_cxx_namespaces()
+    /* FIXME: these only work on ubuntu! */
+    .clang_args(&["-cxx-isystem", "/usr/include/c++/12"])
+    .clang_args(&["-cxx-isystem", "/usr/include/x86_64-linux-gnu/c++/12"])
+    .opaque_type("std::.*")
+    .allowlist_item(".*RE2.*");
+
+  let bindings = bindings.generate().unwrap();
+  bindings.write_to_file(output_path)?;
+
+  Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+  let spack = SpackInvocation::summon().await.unwrap();
+
+  let re2_prefix = utils::ensure_prefix(spack, "re2".into()).await.unwrap();
+
+  link_libraries(re2_prefix.clone()).await.unwrap();
+
+  let libstdcpp_prefix = prefix::Prefix {
+    /* FIXME: this only works on ubuntu! */
+    path: PathBuf::from("/usr/lib/gcc/x86_64-linux-gnu/12"),
+  };
+  let query = prefix::LibsQuery {
+    needed_libraries: vec![prefix::LibraryName("stdc++".to_string())],
+    kind: prefix::LibraryType::Dynamic,
+  };
+  let libs = query.find_libs(&libstdcpp_prefix).await.unwrap();
+  libs.link_libraries();
+
+  let header_path = PathBuf::from("src/re2.hpp");
+  let bindings_path = PathBuf::from("src/bindings.rs");
+  task::spawn_blocking(move || generate_bindings(re2_prefix.path, header_path, bindings_path))
+    .await
+    .unwrap()
+    .unwrap();
+}
---
+++ re2/sys/src/re2.hpp
@@ -0,0 +1,6 @@
+#ifndef __RE2_H__
+#define __RE2_H__
+
+#include "re2/re2.h"
+
+#endif /* __RE2_H__ */
