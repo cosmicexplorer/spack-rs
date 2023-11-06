1d93a566297c6f200898314810b33db72c6e7d98
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2023-11-05 21:42:15 -0500

    attempt to impl scan_matches()

--- hyperscan/src/lib.rs
+++ hyperscan/src/lib.rs
@@ -8,22 +8,35 @@
 #![warn(rustdoc::missing_crate_level_docs)]
 /* Make all doctests fail if they produce any warnings. */
 #![doc(test(attr(deny(warnings))))]
+#![feature(const_nonnull_new)]
+#![feature(const_mut_refs)]
+#![feature(const_pin)]
 
 #[allow(unused, non_camel_case_types)]
 mod bindings;
 
 use bindings as hs;
 
+use async_stream::try_stream;
 use displaydoc::Display;
+use futures_core::stream::Stream;
 use once_cell::sync::Lazy;
+use parking_lot::Mutex;
 use thiserror::Error;
+use tokio::task;
 
 use std::{
   any,
+  collections::VecDeque,
   ffi::CStr,
+  future::Future,
+  marker::PhantomData,
   mem, ops,
-  os::raw::{c_char, c_int, c_uint, c_void},
+  os::raw::{c_char, c_int, c_uint, c_ulonglong, c_void},
+  pin::Pin,
   ptr,
+  sync::Arc,
+  task::{Context, Poll, Waker},
 };
 
 #[derive(
@@ -139,7 +152,7 @@ impl HyperscanError {
     match Self::from_native(x) {
       Ok(()) => Ok(()),
       Err(Self::CompilerError) => {
-        let e = CompileError::copy_from_native(unsafe { &*c });
+        let e = CompileError::copy_from_native(unsafe { &mut *c }).unwrap();
         Err(HyperscanCompileError::Compile(e))
       },
       Err(e) => Err(e.into()),
@@ -511,19 +524,51 @@ impl AsMut<hs::hs_scratch> for Scratch {
   fn as_mut(&mut self) -> &mut hs::hs_scratch { &mut self.0 }
 }
 
-#[derive(Default, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct ScanFlags;
+#[derive(Default, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[repr(transparent)]
+pub struct ScanFlags(c_uint);
 
 impl ScanFlags {
   #[inline]
-  pub const fn into_native(self) -> c_uint { 0 }
+  pub const fn from_native(x: c_uint) -> Self { Self(x) }
+
+  #[inline]
+  pub const fn into_native(self) -> c_uint { self.0 }
 }
 
 /// <expression index {0}>
 #[derive(Debug, Display, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[repr(transparent)]
-pub struct ExpressionIndex(pub c_int);
+pub struct ExpressionIndex(pub c_uint);
 
+impl ExpressionIndex {
+  pub const WHOLE_PATTERN: Self = Self(0);
+}
+
+/// <range index {0}>
+#[derive(Debug, Display, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[repr(transparent)]
+pub struct RangeIndex(pub c_ulonglong);
+
+impl RangeIndex {
+  #[inline(always)]
+  pub const fn into_rust_index(self) -> usize {
+    static_assertions::const_assert!(mem::size_of::<usize>() >= mem::size_of::<c_ulonglong>());
+    self.0 as usize
+  }
+
+  #[inline(always)]
+  pub const fn bounded_range(from: Self, to: Self) -> ops::Range<usize> {
+    static_assertions::assert_eq_size!(ops::Range<usize>, (c_ulonglong, c_ulonglong));
+    let from = from.into_rust_index();
+    let to = to.into_rust_index();
+    debug_assert!(from <= to);
+    ops::Range {
+      start: from,
+      end: to,
+    }
+  }
+}
 
 /// compile error(@{expression}): {message}
 #[derive(Debug, Display, Error)]
@@ -534,18 +579,20 @@ pub struct CompileError {
 
 impl CompileError {
   #[inline]
-  pub fn copy_from_native(x: &hs::hs_compile_error) -> Self {
+  pub fn copy_from_native(x: &mut hs::hs_compile_error) -> Result<Self, HyperscanError> {
     let hs::hs_compile_error {
       message,
       expression,
     } = x;
     assert!(!message.is_null());
-    Self {
+    let ret = Self {
       message: unsafe { CStr::from_ptr(*message) }
         .to_string_lossy()
         .to_string(),
-      expression: ExpressionIndex(*expression),
-    }
+      expression: ExpressionIndex(*expression as c_uint),
+    };
+    HyperscanError::from_native(unsafe { hs::hs_free_compile_error(x) })?;
+    Ok(ret)
   }
 }
 
@@ -571,17 +618,190 @@ impl AsMut<hs::hs_database> for Database {
   fn as_mut(&mut self) -> &mut hs::hs_database { &mut self.0 }
 }
 
-#[repr(transparent)]
-pub struct Context(pub Box<dyn any::Any>);
+#[derive(Debug, Display, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[repr(u8)]
+#[ignore_extra_doc_attributes]
+pub enum MatchResult {
+  /// Continue matching.
+  Continue = 0,
+  /// Immediately cease matching.
+  ///
+  /// If scanning is performed in streaming mode and this value is returned, any
+  /// subsequent calls to @ref hs_scan_stream() for the same stream will
+  /// immediately return with
+  /// [`SCAN_TERMINATED`](HyperscanError::ScanTerminated).
+  CeaseMatching = 1,
+}
 
-impl Context {
+impl MatchResult {
+  /* #[inline] */
+  /* pub const fn */
+
+  /* FIXME: update num_enum so they work with const fn too!!! */
   #[inline]
-  fn mut_ptr_unchecked<T: any::Any>(&mut self) -> *mut T {
-    unsafe { &mut *(&mut self.0 as *mut dyn any::Any as *mut T) }
+  pub const fn from_native(x: c_int) -> Self {
+    if x == 0 {
+      Self::Continue
+    } else {
+      Self::CeaseMatching
+    }
   }
 
   #[inline]
-  pub fn as_native(&mut self) -> *mut c_void { self.mut_ptr_unchecked::<c_void>() }
+  pub const fn into_native(self) -> c_int {
+    match self {
+      Self::Continue => 0,
+      Self::CeaseMatching => 1,
+    }
+  }
+}
+
+#[derive(Debug)]
+pub struct MatchEvent {
+  pub id: ExpressionIndex,
+  pub range: ops::Range<usize>,
+  pub flags: ScanFlags,
+  pub context: Option<ptr::NonNull<c_void>>,
+}
+
+impl MatchEvent {
+  #[inline(always)]
+  pub const fn coerce_args(
+    id: c_uint,
+    from: c_ulonglong,
+    to: c_ulonglong,
+    flags: c_uint,
+    context: *mut c_void,
+  ) -> Self {
+    static_assertions::assert_eq_size!(c_uint, ExpressionIndex);
+    Self {
+      id: ExpressionIndex(id),
+      range: RangeIndex::bounded_range(RangeIndex(from), RangeIndex(to)),
+      flags: ScanFlags::from_native(flags),
+      context: ptr::NonNull::new(context),
+    }
+  }
+
+  #[inline]
+  pub const unsafe fn extract_context<'a, T>(
+    context: Option<ptr::NonNull<c_void>>,
+  ) -> Option<Pin<&'a mut T>> {
+    match context {
+      None => None,
+      Some(c) => Some(Pin::new_unchecked(&mut *mem::transmute::<_, *mut T>(
+        c.as_ptr(),
+      ))),
+    }
+  }
+}
+
+pub type ByteSlice<'a> = &'a [c_char];
+
+#[derive(Debug)]
+pub struct Match<'a> {
+  pub id: ExpressionIndex,
+  pub source: ByteSlice<'a>,
+  pub flags: ScanFlags,
+}
+
+pub struct SliceMatcher<'data, 'code> {
+  parent_slice: ByteSlice<'data>,
+  matched_slices_queue: Mutex<VecDeque<Match<'data>>>,
+  handler: &'code mut dyn FnMut(&Match<'data>) -> MatchResult,
+  wakers: Mutex<Vec<Waker>>,
+}
+
+impl<'data, 'code> SliceMatcher<'data, 'code> {
+  #[inline]
+  pub fn new<F: FnMut(&Match<'data>) -> MatchResult>(
+    parent_slice: ByteSlice<'data>,
+    f: &'code mut F,
+  ) -> Self {
+    Self {
+      parent_slice,
+      matched_slices_queue: Mutex::new(VecDeque::new()),
+      handler: f,
+      wakers: Mutex::new(Vec::new()),
+    }
+  }
+
+  #[inline(always)]
+  const fn parent(&self) -> ByteSlice<'data> { self.parent_slice }
+
+  /* #[inline(always)] */
+  /* pub fn is_empty(&self) -> bool { self.matched_slices_queue.is_empty() } */
+
+  #[inline(always)]
+  pub fn pop(&mut self) -> Option<Match<'data>> { self.matched_slices_queue.lock().pop_front() }
+
+  pub fn pop_rest(&mut self) -> Vec<Match<'data>> {
+    self.matched_slices_queue.lock().drain(..).collect()
+  }
+
+  #[inline(always)]
+  pub fn index_range(&self, range: ops::Range<usize>) -> ByteSlice<'data> { &self.parent()[range] }
+
+  #[inline(always)]
+  pub fn push_new_match(&mut self, m: Match<'data>) {
+    self.matched_slices_queue.lock().push_back(m);
+    for waker in self.wakers.lock().drain(..) {
+      waker.wake();
+    }
+  }
+
+  /* pub fn handle_match(&self, m: Match<'data>) -> MatchResult { */
+  /* let x = 3; */
+  /* m */
+  /* } */
+
+  #[inline(always)]
+  pub fn handle_match(&mut self, m: &Match<'data>) -> MatchResult { (self.handler)(m) }
+
+  #[inline(always)]
+  pub fn push_waker(&mut self, w: Waker) { self.wakers.lock().push(w); }
+}
+
+impl<'data, 'code> Future for SliceMatcher<'data, 'code> {
+  type Output = Match<'data>;
+
+  fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+    if let Some(m) = self.pop() {
+      Poll::Ready(m)
+    } else {
+      self.push_waker(cx.waker().clone());
+      Poll::Pending
+    }
+  }
+}
+
+pub unsafe extern "C" fn match_slice_ref(
+  id: c_uint,
+  from: c_ulonglong,
+  to: c_ulonglong,
+  flags: c_uint,
+  context: *mut c_void,
+) -> c_int {
+  let MatchEvent {
+    id,
+    range,
+    flags,
+    context,
+  } = MatchEvent::coerce_args(id, from, to, flags, context);
+  let mut slice_matcher: Pin<&mut SliceMatcher> =
+    MatchEvent::extract_context::<'_, SliceMatcher>(context).unwrap();
+  let matched_substring = slice_matcher.index_range(range);
+  let m = Match {
+    id,
+    source: matched_substring,
+    flags,
+  };
+
+  let result = slice_matcher.handle_match(&m);
+  if result == MatchResult::Continue {
+    slice_matcher.push_new_match(m);
+  }
+
+  result.into_native()
 }
 
 impl Database {
@@ -620,24 +840,45 @@ impl Database {
     Ok(Self(unsafe { db.assume_init() }))
   }
 
-  pub fn scan(
+  pub fn scan_matches<'data, F: FnMut(&Match<'data>) -> MatchResult+'data>(
     &self,
-    data: &[c_char],
+    data: &'data [c_char],
     flags: ScanFlags,
     scratch: &mut Scratch,
-    /* context: &mut Context, */
-  ) -> Result<(), HyperscanError> {
-    HyperscanError::from_native(unsafe {
-      hs::hs_scan(
-        self.as_ref(),
-        data.as_ptr(),
-        data.len() as c_uint,
-        flags.into_native(),
-        scratch.as_mut(),
-        None,
-        ptr::null_mut(),
-      )
-    })
+    mut f: F,
+  ) -> impl Stream<Item=Result<Match<'data>, HyperscanError>>+'data {
+    let f: &'static mut u8 = unsafe { mem::transmute(&mut f) };
+    let mut matcher = SliceMatcher::new::<F>(data, unsafe { mem::transmute(f) });
+    let s: &hs::hs_database = self.as_ref();
+    let s: usize = unsafe { mem::transmute(s) };
+    let data_len: usize = data.len();
+    let data: usize = unsafe { mem::transmute(data.as_ptr()) };
+    let scratch: &mut hs::hs_scratch = scratch.as_mut();
+    let scratch: usize = unsafe { mem::transmute(scratch) };
+    let ctx: usize = unsafe { mem::transmute(&mut matcher) };
+    let scan_task = task::spawn_blocking(move || {
+      HyperscanError::from_native(unsafe {
+        hs::hs_scan(
+          s as *const hs::hs_database,
+          data as *const c_char,
+          data_len as c_uint,
+          flags.into_native(),
+          scratch as *mut hs::hs_scratch,
+          Some(match_slice_ref),
+          ctx as *mut c_void,
+        )
+      })
+    });
+    try_stream! {
+      let mut matcher = Pin::new(&mut matcher);
+      while !scan_task.is_finished() {
+        yield matcher.as_mut().await;
+      }
+      for m in matcher.pop_rest().into_iter() {
+        yield m;
+      }
+      scan_task.await.unwrap()?;
+    }
   }
 
   pub fn scan_vector(
