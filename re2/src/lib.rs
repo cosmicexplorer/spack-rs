b81497ed8cf3be3c01cfbc9ad12ee445136d74aa
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2023-12-10 00:52:19 -0500

    move named group stuff into group.rs

--- re2/src/lib.rs
+++ re2/src/lib.rs
@@ -20,13 +20,15 @@ use options::{Anchor, Options};
 pub mod string;
 use string::{StringView, StringWrapper};
 
+pub mod group;
+use group::{NamedAndNumberedGroups, NamedCapturingGroups, NamedGroup};
+
 pub mod set;
 
 pub mod filtered;
 
 use std::{
   cmp, fmt, hash,
-  marker::PhantomData,
   mem::{self, MaybeUninit},
   ops, ptr, str,
 };
@@ -42,167 +44,6 @@ unsafe fn array_assume_init<T: Sized, const N: usize>(x: [MaybeUninit<T>; N]) ->
   let y: *const [T; N] = mem::transmute(x);
   ptr::read(y)
 }
-
-#[derive(Copy, Clone)]
-#[repr(transparent)]
-pub struct NamedGroup<'a> {
-  inner: re2_c::NamedGroup,
-  _ph: PhantomData<&'a u8>,
-}
-
-impl<'a> NamedGroup<'a> {
-  #[inline]
-  pub(crate) const unsafe fn from_native(inner: re2_c::NamedGroup) -> Self {
-    Self {
-      inner,
-      _ph: PhantomData,
-    }
-  }
-
-  #[inline]
-  pub const fn name(&self) -> StringView<'a> {
-    unsafe { StringView::from_native(self.inner.name_) }
-  }
-
-  #[inline]
-  pub const fn index(&self) -> &'a usize { unsafe { mem::transmute(&self.inner.index_) } }
-}
-
-impl<'a> fmt::Debug for NamedGroup<'a> {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    write!(f, "NamedGroup(i={}, name={:?})", self.index(), self.name())
-  }
-}
-
-#[repr(transparent)]
-struct NamedCapturingGroups<'a> {
-  inner: re2_c::NamedCapturingGroups,
-  _ph: PhantomData<&'a u8>,
-}
-
-impl<'a> NamedCapturingGroups<'a> {
-  #[inline]
-  const unsafe fn from_native(inner: re2_c::NamedCapturingGroups) -> Self {
-    Self {
-      inner,
-      _ph: PhantomData,
-    }
-  }
-
-  #[inline]
-  fn deref(&self) -> NamedGroup<'a> {
-    let mut out: MaybeUninit<re2_c::NamedGroup> = MaybeUninit::uninit();
-    unsafe {
-      self.inner.deref(out.as_mut_ptr());
-      NamedGroup::from_native(out.assume_init())
-    }
-  }
-
-  #[inline]
-  fn advance(&mut self) {
-    unsafe {
-      self.inner.advance();
-    }
-  }
-
-  #[inline]
-  fn completed(&self) -> bool { unsafe { self.inner.completed() } }
-}
-
-impl<'a> Iterator for NamedCapturingGroups<'a> {
-  type Item = NamedGroup<'a>;
-
-  fn next(&mut self) -> Option<Self::Item> {
-    if self.completed() {
-      return None;
-    }
-
-    let ret = self.deref();
-    self.advance();
-    Some(ret)
-  }
-}
-
-struct NamedAndNumberedGroups<'a> {
-  at_start: bool,
-  total_num_captures: usize,
-  groups_iter: Option<NamedCapturingGroups<'a>>,
-  next_named_group: Option<NamedGroup<'a>>,
-  cur_index: usize,
-}
-
-impl<'a> NamedAndNumberedGroups<'a> {
-  const fn remaining(&self) -> usize {
-    if self.at_start {
-      self.total_num_captures + 1
-    } else {
-      self.total_num_captures - self.cur_index + 1
-    }
-  }
-}
-
-impl<'a> Iterator for NamedAndNumberedGroups<'a> {
-  type Item = Option<&'a str>;
-
-  fn next(&mut self) -> Option<Self::Item> {
-    let Self {
-      ref mut at_start,
-      ref total_num_captures,
-      ref mut groups_iter,
-      ref mut next_named_group,
-      ref mut cur_index,
-    } = self;
-
-    /* Return 0th submatch (for whole pattern). */
-    if *at_start {
-      *at_start = false;
-      *cur_index = 1;
-      return Some(None);
-    }
-
-    if *cur_index > *total_num_captures {
-      return None;
-    }
-
-    if next_named_group.is_none() {
-      let reset_groups_iter = if let Some(ref mut g) = groups_iter {
-        if g.completed() {
-          true
-        } else {
-          *next_named_group = Some(g.deref());
-          g.advance();
-          false
-        }
-      } else {
-        false
-      };
-      if reset_groups_iter {
-        *groups_iter = None;
-      }
-    }
-    let ret = if let Some(named_group) = next_named_group {
-      if *cur_index < *named_group.index() {
-        None
-      } else {
-        debug_assert_eq!(cur_index, named_group.index());
-        Some(named_group.name().as_str())
-      }
-    } else {
-      None
-    };
-    if ret.is_some() {
-      *next_named_group = None;
-    }
-
-    *cur_index += 1;
-
-    Some(ret)
-  }
-
-  fn size_hint(&self) -> (usize, Option<usize>) { (self.remaining(), Some(self.remaining())) }
-}
-
-impl<'a> ExactSizeIterator for NamedAndNumberedGroups<'a> {}
 
 #[repr(transparent)]
 pub struct RE2(re2_c::RE2Wrapper);
@@ -220,7 +61,8 @@ impl RE2 {
   /// # }
   /// ```
   #[inline]
-  pub fn quote_meta(pattern: StringView<'_>) -> StringWrapper {
+  pub fn quote_meta(pattern: &str) -> StringWrapper {
+    let pattern = StringView::from_str(pattern);
     let mut out = StringWrapper::from_view(pattern);
     unsafe { re2_c::RE2Wrapper::quote_meta(pattern.into_native(), out.as_mut_native()) };
     out
@@ -235,7 +77,8 @@ impl RE2 {
   /// assert_eq!(3, RE2::max_submatch(r"\3a\1sdf".into()));
   /// ```
   #[inline]
-  pub fn max_submatch(rewrite: StringView<'_>) -> usize {
+  pub fn max_submatch(rewrite: &str) -> usize {
+    let rewrite = StringView::from_str(rewrite);
     unsafe { re2_c::RE2Wrapper::max_submatch(rewrite.into_native()) }
   }
 
@@ -283,9 +126,7 @@ impl RE2 {
   /// # }
   /// ```
   #[inline]
-  pub fn expensive_clone(&self) -> Self {
-    Self::compile(StringView::from_str(self.pattern()), self.options()).unwrap()
-  }
+  pub fn expensive_clone(&self) -> Self { Self::compile(self.pattern(), self.options()).unwrap() }
 
   #[inline]
   fn error(&self) -> StringView<'_> { unsafe { StringView::from_native(self.0.error()) } }
@@ -301,7 +142,8 @@ impl RE2 {
     })
   }
 
-  pub fn compile(pattern: StringView<'_>, options: Options) -> Result<Self, CompileError> {
+  pub fn compile(pattern: &str, options: Options) -> Result<Self, CompileError> {
+    let pattern = StringView::from_str(pattern);
     let s = Self(unsafe { re2_c::RE2Wrapper::new(pattern.into_native(), &options.into_native()) });
     s.check_error()?;
     Ok(s)
@@ -361,13 +203,7 @@ impl RE2 {
   /// # }
   #[inline]
   pub fn named_and_numbered_groups(&self) -> impl Iterator<Item=Option<&str>>+ExactSizeIterator {
-    NamedAndNumberedGroups {
-      at_start: true,
-      total_num_captures: self.num_captures(),
-      groups_iter: Some(self.make_named_groups()),
-      next_named_group: None,
-      cur_index: 0,
-    }
+    NamedAndNumberedGroups::new(self.num_captures(), self.make_named_groups())
   }
 
   #[inline]
@@ -960,9 +796,7 @@ impl str::FromStr for RE2 {
   ///   },
   /// );
   /// ```
-  fn from_str(s: &str) -> Result<Self, Self::Err> {
-    Self::compile(StringView::from_str(s), Options::default())
-  }
+  fn from_str(s: &str) -> Result<Self, Self::Err> { Self::compile(s, Options::default()) }
 }
 
 impl fmt::Debug for RE2 {
