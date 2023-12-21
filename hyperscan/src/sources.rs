80414204bf37fbf29b5bade07d0738a0c9a468ad
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2023-12-21 14:41:00 -0500

    introduce a tentative architecture for ByteSlice docs

--- hyperscan/src/sources.rs
+++ hyperscan/src/sources.rs
@@ -1,36 +1,42 @@
 /* Copyright 2022-2023 Danny McClanahan */
 /* SPDX-License-Identifier: BSD-3-Clause */
 
+//! Wrappers for types of string data which can be searched and indexed.
+//!
+//! These objects are used to provide string data for inputs to search functions
+//! like [`scan_sync()`](crate::state::Scratch::scan_sync), and subsets of those
+//! arguments are received as outputs by match callbacks to produce match info
+//! such as [`Match`](crate::matchers::contiguous_slice::Match).
+
 use std::{
   cmp, mem, ops,
   os::raw::{c_char, c_uint},
   slice, str,
 };
 
+/// A [`slice`](prim@slice) of [`u8`] with an associated lifetime.
 #[derive(Debug, Copy, Clone)]
 #[repr(transparent)]
 pub struct ByteSlice<'a>(&'a [u8]);
 
+/// # Byte-Oriented Interface
+/// Hyperscan can search over arbitrary byte patterns in any encoding, so
+/// [`Self::from_slice()`] and [`Self::as_slice()`] offer the most general
+/// byte-oriented interface. This may be particularly useful when matching
+/// against non-UTF8 data, possibly with [`Literal`](crate::expression::Literal)
+/// pattern strings (although non-literal patterns can also be used to
+/// match non-UTF8 data).
+///
+/// [`From`] implementations are also provided to convert from native
+/// [arrays](prim@array) and [slices](prim@slice).
 impl<'a> ByteSlice<'a> {
-  pub fn index_range(&self, range: impl slice::SliceIndex<[u8], Output=[u8]>) -> Option<Self> {
-    self.as_slice().get(range).map(Self::from_slice)
-  }
-
-  pub const fn from_str(data: &'a str) -> Self { Self::from_slice(data.as_bytes()) }
-
   pub const fn from_slice(data: &'a [u8]) -> Self { Self(data) }
 
   pub const fn as_slice(&self) -> &'a [u8] { self.0 }
 
-  pub const unsafe fn as_str(&self) -> &'a str { str::from_utf8_unchecked(self.as_slice()) }
-
   pub(crate) const fn as_ptr(&self) -> *const c_char { self.as_slice().as_ptr() as *const c_char }
 
-  pub const fn len(&self) -> usize { self.as_slice().len() }
-
-  pub const fn is_empty(&self) -> bool { self.len() == 0 }
-
-  pub(crate) const fn native_len(&self) -> c_uint { self.len() as c_uint }
+  pub(crate) const fn native_len(&self) -> c_uint { self.as_slice().len() as c_uint }
 }
 
 impl<'a> From<&'a [u8]> for ByteSlice<'a> {
@@ -41,8 +47,41 @@ impl<'a, const N: usize> From<&'a [u8; N]> for ByteSlice<'a> {
   fn from(x: &'a [u8; N]) -> Self { Self::from_slice(x.as_ref()) }
 }
 
+/// # String-Oriented Interface
+/// When hyperscan is being used with UTF8-encoded inputs (e.g. with
+/// [`Self::from_str()`]), it will produce UTF8 encoded match outputs, and
+/// [`Self::as_str()`] can be invoked safely on match results.
+///
+/// A [`From`] implementation is also provided to convert from a native
+/// [`str`](prim@str).
+///
+/// ## The `UTF8` Flag
+/// It is important to note that hyperscan itself does not assume any particular
+/// string encoding, and the function of e.g.
+/// [`Flags::UTF8`](crate::flags::Flags::UTF8) is to determine which bytes
+/// should be included in the state machine, *not* the encoding of any
+/// particular input. This means that the UTF8 flag may be disabled for UTF8
+/// inputs to produce a much smaller state machine (as it is when using
+/// [`Flags::default()`](crate::flags::Flags::default)). Note however that
+/// enabling the UTF8 flag for non-UTF8 inputs produces undefined behavior.
+impl<'a> ByteSlice<'a> {
+  pub const fn from_str(data: &'a str) -> Self { Self::from_slice(data.as_bytes()) }
+
+  pub const unsafe fn as_str(&self) -> &'a str { str::from_utf8_unchecked(self.as_slice()) }
+}
+
 impl<'a> From<&'a str> for ByteSlice<'a> {
   fn from(x: &'a str) -> Self { Self::from_str(x) }
+}
+
+/// # Subsetting
+/// Match callbacks return subsets of the input argument. These methods apply a
+/// fallible subsetting operation which is used to convert match offsets to
+/// substrings.
+impl<'a> ByteSlice<'a> {
+  pub fn index_range(&self, range: impl slice::SliceIndex<[u8], Output=[u8]>) -> Option<Self> {
+    self.as_slice().get(range).map(Self::from_slice)
+  }
 }
 
 #[derive(Debug, Copy, Clone)]
